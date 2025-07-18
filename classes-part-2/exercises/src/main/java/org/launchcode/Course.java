bb6aa248fefa88f991eaeaea6cd8adff317372b3
katytapley <katy.tapley@gmail.com>
2025-07-18 16:11:18 -0500

    attempted exercises

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     private String topic;
@@ -9,9 +10,25 @@ public class Course {
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+    public String toString() {
+        return "The course, " + topic + " will be taught by, " + instructor + " and these students are currently " +
+                "enrolled: " + enrolledStudents;
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course course)) return false;
+        return Objects.equals(topic, course.topic) && Objects.equals(instructor, course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topic, instructor);
+    }
 }
